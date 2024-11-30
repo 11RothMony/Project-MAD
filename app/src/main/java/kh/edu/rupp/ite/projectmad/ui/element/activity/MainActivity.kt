fbf530d28330863552802b4601bfe476024367b3
anes <anesmai004@gmail.com>
2024-11-30 23:42:57 +0700

    implement project

--- app/src/main/java/kh/edu/rupp/ite/projectmad/ui/element/activity/MainActivity.kt
+++ app/src/main/java/kh/edu/rupp/ite/projectmad/ui/element/activity/MainActivity.kt
@@ -1,24 +1,60 @@
 package kh.edu.rupp.ite.projectmad.ui.element.activity
 
-import android.app.Activity
-import kh.edu.rupp.ite.projectmad.ui.element.fragment.FragmentLogin
-import androidx.appcompat.app.AppCompatActivity
+import HomeFragment
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.fragment.app.Fragment
 import kh.edu.rupp.ite.projectmad.R
-import kh.edu.rupp.ite.projectmad.ui.element.fragment.FragmentMenu
+import kh.edu.rupp.ite.projectmad.databinding.ActivityMainBinding
+
 import kh.edu.rupp.ite.visitme.ui.element.activity.BaseActivity
 
 class MainActivity : BaseActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val homeFragment = HomeFragment()
+    private lateinit var activeFragment: Fragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
 
-
-
-        val fragment = FragmentMenu()
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentMenu, fragment)
-            .commit()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupFragment()
+        setupBottomNavBar()
 
     }
+
+    private fun setupFragment() {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        activeFragment = homeFragment
+
+        fragmentTransaction.add(binding.lyFragment.id, homeFragment)
+
+        fragmentTransaction.commit()
+    }
+
+    private fun setupBottomNavBar() {
+        binding.bottomNavigation.setOnItemSelectedListener {
+            handleOnNavigationItemSelected(it)
+        }
+    }
+
+    private fun handleOnNavigationItemSelected(item: MenuItem): Boolean{
+        when (item.itemId){
+            R.id.menuHome -> showFragement(homeFragment)
+        }
+        return true
+    }
+
+    private fun showFragement(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.hide(activeFragment)
+        fragmentTransaction.show(fragment)
+        activeFragment = fragment
+        fragmentTransaction.commit()
+    }
+
 }
