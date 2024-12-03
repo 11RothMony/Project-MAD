d68350c3218436096405f24609436f12009ca4fa
anes <anesmai004@gmail.com>
2024-12-03 22:21:53 +0700

    implement home page and make menu on pizza

--- app/src/main/java/kh/edu/rupp/ite/projectmad/ui/element/fragment/CartFragment.kt
+++ app/src/main/java/kh/edu/rupp/ite/projectmad/ui/element/fragment/CartFragment.kt
@@ -4,16 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import kh.edu.rupp.ite.projectmad.R
 
-class FragmentLogin : BaseFragment() {
+class CartFragment : BaseFragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_cart, container, false)
     }
 
 }
