701769329a4e3b67902e9c04bf90e1cb6cf35ea3
anes <anesmai004@gmail.com>
2024-12-08 21:19:12 +0700

    make flavor on app and put firebase in project

--- app/src/main/java/kh/edu/rupp/ite/projectmad/global/AppConstant.kt
+++ app/src/main/java/kh/edu/rupp/ite/projectmad/global/AppConstant.kt
@@ -2,8 +2,9 @@ package kh.edu.rupp.ite.projectmad.global
 
 class AppConstant {
 
-    companion object{
-        val API_BASE_URL = "https://dummyjson.com/c/"
+    companion object {
+//        val API_BASE_URL = "https://dummyjson.com/c/"
+//        val API_BASE_URL = "http://127.0.0.1:3000/"
         val API_STATUS_SUCCESS = "success"
     }
 }
