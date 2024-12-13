6051ac6942ef512eefd24625f8fcb45a4e126efc
anes <anesmai004@gmail.com>
2024-12-13 22:41:10 +0700

    make login use firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,6 +83,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-database:21.0.0")
+    implementation("com.google.firebase:firebase-auth-ktx:23.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -99,5 +100,8 @@ dependencies {
 
     // Import the BoM for the Firebase platform
     implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+//    implementation ("com.google.firebase:firebase-auth-ktx")
+//    implementation ("com.google.firebase:firebase-functions-ktx")
+
 
 }
--- app/src/main/java/kh/edu/rupp/ite/projectmad/ui/element/fragment/AccountFragment.kt
+++ app/src/main/java/kh/edu/rupp/ite/projectmad/ui/element/fragment/AccountFragment.kt
@@ -1,31 +1,90 @@
 package kh.edu.rupp.ite.projectmad.ui.element.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.firebase.Firebase
-import com.google.firebase.database.database
+import android.widget.Button
+import android.widget.RelativeLayout
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kh.edu.rupp.ite.projectmad.R
+import kh.edu.rupp.ite.projectmad.ui.element.activity.LoginActivity
+import kh.edu.rupp.ite.projectmad.ui.element.activity.MainActivity
 
 class AccountFragment : BaseFragment() {
+
+    private lateinit var allProfileFragment: RelativeLayout
+
+    //    private lateinit var coverImageView: ImageView
+    private lateinit var loginButton: Button
+    private lateinit var signOutButton: Button
+
+    private lateinit var auth: FirebaseAuth
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         return inflater.inflate(R.layout.fragment_account, container, false)
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // Write a message to the database
-        val database = Firebase.database
-        val myRef = database.getReference("message")
 
-        myRef.setValue("Hello, World!")
+
+        allProfileFragment = view.findViewById(R.id.allProfileFragment)
+        loginButton = view.findViewById(R.id.myButton)
+        signOutButton = view.findViewById(R.id.btnSignOut)
+
+
+        auth = FirebaseAuth.getInstance()
+
+        val currentUser = auth.currentUser
+
+        if (currentUser != null) {
+            showProfile(currentUser)
+            showButtonSignOut()
+        } else {
+            showButtonLogin()
+        }
+
+
     }
 
+
+    private fun showProfile(user: FirebaseUser) {
+
+    }
+
+
+    private fun showButtonLogin() {
+        Toast.makeText(requireContext(), "Function executed!", Toast.LENGTH_SHORT).show()
+
+        allProfileFragment.visibility = View.GONE
+
+        loginButton.setOnClickListener {
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    private fun showButtonSignOut() {
+
+        signOutButton.setOnClickListener {
+            auth.signOut()
+
+            startActivity(Intent(requireContext(), LoginActivity::class.java))
+
+        }
+
+    }
 
 
 }
