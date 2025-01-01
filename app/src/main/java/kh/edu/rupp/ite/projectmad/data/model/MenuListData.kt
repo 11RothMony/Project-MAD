6c156f7f24b64fa0ce13ed33c4fc1f39737f3628
anes <anesmai004@gmail.com>
2025-01-01 15:49:37 +0700

    make add product into cart fragment

--- app/src/main/java/kh/edu/rupp/ite/projectmad/data/model/MenuListData.kt
+++ app/src/main/java/kh/edu/rupp/ite/projectmad/data/model/MenuListData.kt
@@ -7,4 +7,5 @@ data class MenuListData(
     val price: Double,
     val size: String,
     val isVegetarian: Boolean,
+    val quantity: Int = 1,
 )
--- app/src/main/java/kh/edu/rupp/ite/projectmad/ui/viewmodel/CartViewModel.kt
+++ app/src/main/java/kh/edu/rupp/ite/projectmad/ui/viewmodel/CartViewModel.kt
@@ -3,23 +3,18 @@ package kh.edu.rupp.ite.projectmad.ui.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kh.edu.rupp.ite.projectmad.data.model.CartData
+import kh.edu.rupp.ite.projectmad.data.model.CartManager
+import kh.edu.rupp.ite.projectmad.data.model.MenuListData
 
 class CartViewModel : ViewModel() {
 
-    private val _cart = MutableLiveData<MutableList<CartData>>(mutableListOf())
-    val cart: LiveData<MutableList<CartData>> get() = _cart
+    private val _cartItems = MutableLiveData<List<MenuListData>>(emptyList())
+    val cartItem: LiveData<List<MenuListData>> get() = _cartItems
 
-    fun addToCart(cart : CartData){
+    fun loadItemCart(){
 
-        val currentCart = _cart.value ?: mutableListOf()
-        currentCart.add(cart)
-        _cart.value = currentCart
+        _cartItems.value = CartManager.getCartItems()
 
-    }
-
-    fun getCartItem(): List<CartData>{
-        return _cart.value ?: emptyList()
     }
 
 }
