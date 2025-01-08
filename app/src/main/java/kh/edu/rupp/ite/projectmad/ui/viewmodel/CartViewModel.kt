af21e1c5a367f41614410317071f1c388e498432
anes <anesmai004@gmail.com>
2025-01-08 23:14:39 +0700

    make adapter communicate back to fragment by use RecyclerView adapter

--- app/src/main/java/kh/edu/rupp/ite/projectmad/ui/viewmodel/CartViewModel.kt
+++ app/src/main/java/kh/edu/rupp/ite/projectmad/ui/viewmodel/CartViewModel.kt
@@ -5,34 +5,28 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kh.edu.rupp.ite.projectmad.data.api.client.ApiClient
-import kh.edu.rupp.ite.projectmad.data.model.ApiState
 import kh.edu.rupp.ite.projectmad.data.model.CartManager
 import kh.edu.rupp.ite.projectmad.data.model.MenuListData
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+
 
 class CartViewModel : ViewModel() {
 
-    private val _cartItems = MutableLiveData<List<MenuListData>>(emptyList())
+    private val _cartItems = MutableLiveData<List<MenuListData>>(mutableListOf())
     val cartItem: LiveData<List<MenuListData>> get() = _cartItems
 
     private val _totalPrice = MutableLiveData<Double>()
     val totalPrice: LiveData<Double> get() = _totalPrice
 
-
-    private val _clearItems  = MutableLiveData<List<String>>()
-//    val clearItem: LiveData<List<String>> get() = _clearItems
-
-    private val _itemsLiveData = MutableLiveData<List<MenuListData>>()
-//    val itemsLiveData : LiveData<List<MenuListData>> get() = _itemsLiveData
+    private val _itemsLiveData = MutableLiveData<MutableList<MenuListData>>()
     val itemLiveData get() = _itemsLiveData
 
 
     fun loadItemCart(){
         _cartItems.value = CartManager.getCartItems()
         _totalPrice.value = CartManager.getTotalPrice()
+        Log.d("totalPrice1", "${_totalPrice.value}")
+        Log.d("totalPrice2", "${totalPrice.value}")
     }
 
     init {
@@ -46,12 +40,10 @@ class CartViewModel : ViewModel() {
 
     fun deleteItemById(id: Int) {
         CartManager.deleteItemById(id)
+        Log.d("DeleteItem", "${CartManager.getTotalPrice()}")
         loadItemCart() // Refresh the LiveData with updated cart items
+
     }
-
-
-
-
 
 
 }
